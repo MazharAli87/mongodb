bbbbc9825f3a474a74991a9c5b5854ecb5cc63ab
Mazhar Ali <mazhar852510@gmail.com>
2025-06-18 14:42:47 +0530

    Delete methods

--- delete.js
+++ delete.js
@@ -9,4 +9,16 @@ async function deleteUser() {
   process.exit();
 }
 
+async function deleteMultUsers() {
+  const user = await User.deleteMany({ email: 'test@example.com' });
+  console.log('Users deleted');
+  process.exit();
+}
+
+async function deleteUserById() {
+  const user = await User.findByIdAndDelete('684fd28ad1b85191295b9635');
+  console.log('Users deleted');
+  process.exit();
+}
+
 deleteUser()
